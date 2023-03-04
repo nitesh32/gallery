fedf9589aa69b957186c7f5d515e353944e0aa72
nitesh32 <nit8339@gmail.com>
2023-03-04 11:25:55 +0530

    complete

---
+++ index.js
@@ -0,0 +1,188 @@
+
+
+function one_top(){
+var one=document.getElementById("one");
+var two=document.getElementById("two");
+var three=document.getElementById("three");
+var four=document.getElementById("four");
+var five=document.getElementById("five");
+    // five.setAttribute ('style', 'height: 150px !important;');
+    five.setAttribute ('style', 'z-index: 2 !important;');
+    five.style.height="160px";
+    five.style.left="30px";
+    five.style.transition="1s";
+    five.setAttribute("id","two");
+    
+
+    // four.setAttribute ('style', 'height: 160px !important;');
+    four.setAttribute ('style', 'z-index: 1 !important;');
+    four.style.height="150px";
+    four.style.left="10px";
+    four.style.transition="1s";
+    four.setAttribute("id","one");
+    
+
+    // three.setAttribute ('style', 'height: 150px !important;');
+    three.setAttribute ('style', 'z-index: 1 !important;');
+    three.style.height="150px";
+    three.style.left="90px";
+    three.style.transition="1s";
+    three.setAttribute("id","five");
+    
+    
+    // two.setAttribute ('style', 'height: 160px !important;');
+    two.setAttribute ('style', 'z-index: 2 !important;');
+    two.style.left="70px";
+    two.style.height="160px";
+    two.style.transition="1s";
+    two.setAttribute("id","four");
+    
+
+    // one.setAttribute ('style', 'height: 170px !important;');
+    one.setAttribute ('style', 'z-index: 3 !important;');
+    one.style.left="50px";
+    one.style.height="170px";
+    one.style.transition="1s";
+    one.setAttribute("id","three");
+    
+}
+function two_top(){
+var one=document.getElementById("one");
+var two=document.getElementById("two");
+var three=document.getElementById("three");
+var four=document.getElementById("four");
+var five=document.getElementById("five");
+    // five.setAttribute ('style', 'height: 150px !important;');
+    five.setAttribute ('style', 'z-index: 1 !important;');
+    five.style.height="150px";
+    five.style.left="10px";
+    five.style.transition="1s";
+    five.setAttribute("id","one");
+    
+
+    // four.setAttribute ('style', 'height: 160px !important;');
+    four.setAttribute ('style', 'z-index: 1 !important;');
+    four.style.height="150px";
+    four.style.left="90px";
+    four.style.transition="1s";
+    four.setAttribute("id","five");
+    
+
+    // three.setAttribute ('style', 'height: 150px !important;');
+    three.setAttribute ('style', 'z-index: 2 !important;');
+    three.style.height="160px";
+    three.style.left="70px";
+    three.style.transition="1s";
+    three.setAttribute("id","four");
+    
+    
+    // two.setAttribute ('style', 'height: 160px !important;');
+    two.setAttribute ('style', 'z-index: 3 !important;');
+    two.style.left="50px";
+    two.style.height="170px";
+    two.style.transition="1s";
+    two.setAttribute("id","three");
+    
+
+    // one.setAttribute ('style', 'height: 170px !important;');
+    one.setAttribute ('style', 'z-index: 2 !important;');
+    one.style.left="30px";
+    one.style.height="160px";
+    one.style.transition="1s";
+    one.setAttribute("id","two");
+}
+function four_top(){
+var one=document.getElementById("one");
+var two=document.getElementById("two");
+var three=document.getElementById("three");
+var four=document.getElementById("four");
+var five=document.getElementById("five");
+    // five.setAttribute ('style', 'height: 150px !important;');
+    five.setAttribute ('style', 'z-index: 2 !important;');
+    five.style.height="160px";
+    five.style.left="70px";
+    five.style.transition="1s";
+    five.setAttribute("id","four");
+    
+
+    // four.setAttribute ('style', 'height: 160px !important;');
+    four.setAttribute ('style', 'z-index: 3 !important;');
+    four.style.height="170px";
+    four.style.left="50px";
+    four.style.transition="1s";
+    four.setAttribute("id","three");
+    
+
+    // three.setAttribute ('style', 'height: 150px !important;');
+    three.setAttribute ('style', 'z-index: 2 !important;');
+    three.style.height="160px";
+    three.style.left="30px";
+    three.style.transition="1s";
+    three.setAttribute("id","two");
+    
+    
+    // two.setAttribute ('style', 'height: 160px !important;');
+    two.setAttribute ('style', 'z-index: 1 !important;');
+    two.style.left="10px";
+    two.style.height="150px";
+    two.style.transition="1s";
+    two.setAttribute("id","one");
+    
+
+    // one.setAttribute ('style', 'height: 170px !important;');
+    one.setAttribute ('style', 'z-index: 1 !important;');
+    one.style.left="90px";
+    one.style.height="150px";
+    one.style.transition="1s";
+    one.setAttribute("id","five");
+    
+    
+}
+function five_top(){
+var one=document.getElementById("one");
+var two=document.getElementById("two");
+var three=document.getElementById("three");
+var four=document.getElementById("four");
+var five=document.getElementById("five");
+    // five.setAttribute ('style', 'height: 150px !important;');
+    five.setAttribute ('style', 'z-index: 3 !important;');
+    five.style.height="170px";
+    five.style.left="50px";
+    five.style.transition="1s";
+    five.setAttribute("id","three");
+    
+
+    // four.setAttribute ('style', 'height: 160px !important;');
+    four.setAttribute ('style', 'z-index: 2 !important;');
+    four.style.height="160px";
+    four.style.left="30px";
+    four.style.transition="1s";
+    four.setAttribute("id","two");
+    
+
+    // three.setAttribute ('style', 'height: 150px !important;');
+    three.setAttribute ('style', 'z-index: 1 !important;');
+    three.style.height="150px";
+    three.style.left="10px";
+    three.style.transition="1s";
+    three.setAttribute("id","one");
+    
+    
+    // two.setAttribute ('style', 'height: 160px !important;');
+    two.setAttribute ('style', 'z-index: 2 !important;');
+    two.style.left="70px";
+    two.style.height="160px";
+    two.style.transition="1s";
+    two.setAttribute("id","four");
+    
+
+    // one.setAttribute ('style', 'height: 170px !important;');
+    one.setAttribute ('style', 'z-index: 1 !important;');
+    one.style.left="90px";
+    one.style.height="150px";
+    one.style.transition="1s";
+    one.setAttribute("id","five");
+
+    
+    
+}
